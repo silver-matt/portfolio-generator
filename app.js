58b4d32a768abc45476a483c94cc52439ba37e75
Matt Silver <Mattsilver54@yahoo.com>
2022-05-22 18:25:59 -0700

    added prompt

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer')
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
 
-console.log(profileDataArgs);
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-console.log(name, github);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const pageHTML = generatePage(name, github);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
